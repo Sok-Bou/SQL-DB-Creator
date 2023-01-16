fcb78ffaf00f79120f6fba1c971880cd176d1d29
Sokrates Bousios <sokratesb@gmail.com>
2023-01-16 15:34:06 +0100

    DB connection and Pool Created. First query implemented - toml : actix-web and futures included.

--- sql_db_creator/src/main.rs
+++ sql_db_creator/src/main.rs
@@ -1,10 +1,54 @@
 mod database;
 mod util;
+mod secure;
 
 use crate::database::DB;
 use util::sub_paths;
+use secure::Credentials;
+use futures::executor::block_on;
+
+use sqlx::mysql::MySqlPool;
+use sqlx::Pool;
+use sqlx::MySql;
+use sqlx::Error;
+use sqlx::mysql::MySqlQueryResult;
+
+// use sqlx::postgres::PgPoolOptions;
+
+async fn connect() -> Result<Pool<MySql>, Error> {
+    let credentials = Credentials::new();
+
+    let user = credentials.user;
+    let password = credentials.password;
+    let host = credentials.host;
+
+    println!("User: {user}, Password: {password}, Host: {host}");
+
+    let url = format!("mysql://{user}:{password}@{host}");
+
+    MySqlPool::connect(&url).await
+}
+
+async fn create_db(pool: &Pool<MySql>) -> Result<MySqlQueryResult, Error> {
+    sqlx::query("CREATE DATABASE IF NOT EXISTS test").execute(pool).await
+}
 
 fn main() {
+
+    let pool_future = connect();
+
+    match block_on(pool_future) {
+        Ok(pool) => {
+            match block_on(create_db(&pool)) {
+                Ok(query) => {
+                    println!("Database created: {:?}", query);
+                },
+                Err(e) => println!("Database could't be created: {e}")
+            }
+        },
+        Err(e) => println!("Pool could't be created: {e}")
+    }
+
 
     let db_name_paths = match sub_paths("./src/db/") {
         Ok(paths) => paths,
@@ -21,6 +65,9 @@ fn main() {
 
     for db in dbs {
         println!("{}", db.name);
+
+
+
         for table in db.tables {
             println!("{}", table.name);
             println!("{}", table.path);
